97884cfd67f25fc1b5d80031ef8f82c8052bee0b
Danai Adkisson <da129@duke.edu>
2023-09-18 14:17:22 -0400

    {}

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #echo "term:${USER_PASSWORD}" | chpasswd #USER_PASSWORD is passed in on docker run so it can read from the user mapping
 #/bin/chown -R term:term /home/term
-supervisord -c /etc/supervisor/supervisord.conf
+#supervisord -c /etc/supervisor/supervisord.conf
+/usr/bin/wetty --port 8080 --title colab-bash --ssh-config /etc/ssh/ssh_config --base /
