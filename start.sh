a5ca56529940fc45af5759a5a696a20e84df0ec6
Danai Adkisson <da129@duke.edu>
2023-09-18 15:23:29 -0400

    {try with DNS port }

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 #echo "term:${USER_PASSWORD}" | chpasswd #USER_PASSWORD is passed in on docker run so it can read from the user mapping
 #/bin/chown -R term:term /home/term
 #supervisord -c /etc/supervisor/supervisord.conf
-/opt/node-v18.17.1-linux-x64/bin/wetty --host 0.0.0.0 --port 8080 --ssh-host colab-roots-terminal-dev --ssh-port 22 --ssh-user term --title colab-bash --ssh-config /etc/ssh/ssh_config --base /
+/opt/node-v18.17.1-linux-x64/bin/wetty --host 0.0.0.0 --port 8080 --ssh-host colab-roots-terminal-dev --ssh-port ssh-port --ssh-user term --title colab-bash --ssh-config /etc/ssh/ssh_config --base /
